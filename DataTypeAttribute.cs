ea8e5b1412006af5ad4af479e6bae3b160a4c075
Smith <SmithSt@WSDOT.WA.GOV>
2020-01-09 13:08:24 -0800

    Add project files.

---
+++ DataTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    internal class DataTypeAttribute : Attribute
+    {
+        private DataType date;
+
+        public DataTypeAttribute(DataType date)
+        {
+            this.date = date;
+        }
+    }
+}
